9f972ebaf3a57fbf708872ce331561e439b7df04
m7exb <m77exb@gmail.com>
2024-04-29 15:18:38 -0400

    new changes

---
+++ excersises/simple_functions.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function favoriteThings() {
+  console.log("Muneeb Tasawar");
+  console.log("The Batman");
+  console.log("NAV");
+}
+
+function whyImHere() {
+  console.log("I joined Year up to find new opportunities");
+}
+
+function favoritePlace() {
+  console.log(
+    "My favorite place to visit is Pakistan because thats where my family is from"
+  );
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
